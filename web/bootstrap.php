8b925141d6459cb76a489dcd26ae351f9b9fb509
heckslay <lomidzelf@gmail.com>
2019-08-15 17:08:53 +0400

    Create bootstrapping and markup backbone for the application

---
+++ web/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+use database\Connection;
+
+require_once('../database/Connection.php');
+
+Connection::connectToDatabase();
+?>
+
+<?php require_once 'product-list.php' ?>
+
+
+
+
---
+++ web/product-list.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+    <div class="col-sm-8">
+        <h2>Product List</h2>
+    </div>
+    <div class="col-sm-4">
+        <select>
+            <option value="Mass Delete Action"></option>
+        </select>
+    </div>
+    </div>
+</div>
