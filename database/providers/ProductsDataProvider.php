fd6627b58b26233fdca2c78dd561e7f6a0ba586b
heckslay <lomidzelf@gmail.com>
2019-08-16 20:02:40 +0400

    Create ProductsDataProvider class and it's method getAllProducts Use newly created method to show data from database in product-list view

---
+++ database/providers/ProductsDataProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace database\providers;
+require_once('../database/Connection.php');
+require_once('../models/Product.php');
+require_once('../models/Furniture.php');
+require_once('../models/Book.php');
+require_once('../models/DVDDisk.php');
+
+use database\Connection;
+use models\Product;
+use models\Furniture;
+use models\Book;
+use models\DVDDisk;
+use PDO;
+
+
+class ProductsDataProvider
+{
+    public static function getAllProducts()
+    {
+        $connection = Connection::connectToDatabase();
+        $allProductsPrep = $connection->prepare('SELECT * FROM products 
+                                                           left join product_types 
+                                                               on products.product_type_id = product_types.id 
+                                                           WHERE deleted_at is null');
+        $allProductsPrep->execute();
+        $result = $allProductsPrep->setFetchMode(PDO::FETCH_ASSOC);
+        $allProductsArr = $allProductsPrep->fetchAll();
+        $allProductsObjArr = [];
+        return $allProductsArr;
+
+    }
+}
--- views/product-list.php
+++ views/product-list.php
@@ -1,138 +1,62 @@
+<?php
+/** @var [] $allProductsArr */
+require_once('../database/providers/ProductsDataProvider.php');
+
+use database\providers\ProductsDataProvider;
+use models\Product;
+
+$allProductsArr = ProductsDataProvider::getAllProducts();
+?>
 <div class="container product-list-container">
     <div class="row title-row">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-7 responsive-col">
             <h2>Product List</h2>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 responsive-col">
-                <select class="form-control">
-                    <option value="Mass Delete Action">Mass Delete Action</option>
-                </select>
+            <select class="form-control">
+                <option value="Mass Delete Action">Mass Delete Action</option>
+            </select>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-3 col-lg-2 responsive-col">
             <button class="btn btn-primary btn-danger">Apply</button>
-            <a href="index.php?pageName=add-product"><button class="btn btn-primary">Add</button></a>
+            <a href="index.php?pageName=add-product">
+                <button class="btn btn-primary">Add</button>
+            </a>
         </div>
     </div>
     <div class="row product-row">
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                <label class="form-check-label" for="itemCheck">
-                </label>
-            </div>
-            <ul class="single-product-info">
-                <li class="product-sku">JVC200123</li>
-                <li class="product-title">ACME Disk</li>
-                <li class="product-price">1.00$</li>
-                <li class="product-special-property">Size: 700MB</li>
-            </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
+        <?php if ($allProductsArr): ?>
+            <?php foreach ($allProductsArr as $singleProduct): ?>
+                <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
+                    <div class="single-product-box">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                            <label class="form-check-label" for="itemCheck">
+                            </label>
+                        </div>
+                        <ul class="single-product-info">
+                            <li class="product-sku"><?php echo $singleProduct['sku']; ?></li>
+                            <li class="product-name"><?php echo $singleProduct['name']; ?></li>
+                            <li class="product-price"><?php echo $singleProduct['price']; ?></li>
+                            <li class="product-special-property">
+                                <?php
+                                if($singleProduct['product_type_id'] == Product::TYPE_BOOK) {
+                                    echo 'Weight: ' . $singleProduct['weight'];
+                                } else if($singleProduct['product_type_id'] == Product::TYPE_DVD) {
+                                    echo 'Size: ' . $singleProduct['size'];
+                                } else if($singleProduct['product_type_id'] == Product::TYPE_FURNITURE) {
+                                    echo 'Dimension: ' . $singleProduct['dimensions'];
+                                }
+                                ?>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <p class="no-entries-found">No Entries Found In Database</p>
             </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-xs-1 col-sm-4 col-md-3 col-lg-3">
-            <div class="single-product-box">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
-                    <label class="form-check-label" for="itemCheck">
-                    </label>
-                </div>
-                <ul class="single-product-info">
-                    <li class="product-sku">JVC200123</li>
-                    <li class="product-title">ACME Disk</li>
-                    <li class="product-price">1.00$</li>
-                    <li class="product-special-property">Size: 700MB</li>
-                </ul>
-            </div>
-        </div>
+        <?php endif; ?>
     </div>
 </div>
