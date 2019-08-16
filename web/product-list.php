573ae4ca3a1210b392da90f7f78e52142b7307ab
heckslay <lomidzelf@gmail.com>
2019-08-16 13:24:57 +0400

    Implement basic layout for the product list page (Static at this point) Add favicon.ico Import jQuery and custom styles.css in project

--- web/product-list.php
+++ web/product-list.php
@@ -1,12 +1,137 @@
-<div class="container">
-    <div class="row">
-    <div class="col-sm-8">
-        <h2>Product List</h2>
+<div class="container-fluid main-container">
+    <div class="row title-row">
+        <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 responsive-col">
+            <h2>Product List</h2>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 responsive-col">
+                <select class="form-control">
+                    <option value="Mass Delete Action">Mass Delete Action</option>
+                </select>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 responsive-col">
+            <button class="btn btn-primary">Apply</button>
+        </div>
     </div>
-    <div class="col-sm-4">
-        <select>
-            <option value="Mass Delete Action"></option>
-        </select>
-    </div>
+    <div class="row product-row">
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                <label class="form-check-label" for="itemCheck">
+                </label>
+            </div>
+            <ul class="single-product-info">
+                <li class="product-sku">JVC200123</li>
+                <li class="product-title">ACME Disk</li>
+                <li class="product-price">1.00$</li>
+                <li class="product-special-property">Size: 700MB</li>
+            </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-1 col-sm-3 col-md-3 col-lg-3">
+            <div class="single-product-box">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="itemCheck">
+                    <label class="form-check-label" for="itemCheck">
+                    </label>
+                </div>
+                <ul class="single-product-info">
+                    <li class="product-sku">JVC200123</li>
+                    <li class="product-title">ACME Disk</li>
+                    <li class="product-price">1.00$</li>
+                    <li class="product-special-property">Size: 700MB</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
