e6e038b50925638de40f4fba58f8727cbc1bb587
heckslay <lomidzelf@gmail.com>
2019-08-16 20:00:42 +0400

    Update migrations to insert initial testing values in both tables

--- database/migrations/CreateProductTypesTable.php
+++ database/migrations/CreateProductTypesTable.php
@@ -15,9 +15,14 @@ class CreateProductTypesTable
             $connection = Connection::connectToDatabase();
             $createTableSql = 'CREATE TABLE product_types (
                             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-                            type VARCHAR(70) NOT NULL)';
+                            type VARCHAR(70) NOT NULL);
+                            INSERT INTO product_types VALUES (1, \'Book\');
+INSERT INTO product_types VALUES (2, \'DVD Disk\');
+INSERT INTO product_types VALUES (3, \'Furniture\');
+';
             $connection->exec($createTableSql);
-            echo 'Created Table Product Types Successfully'. PHP_EOL;
+
+            echo 'Created Table Product Types and Inserted Initial Values Successfully'. PHP_EOL;
         } catch (\PDOException $e) {
             echo 'Failed To Create Product Types Table: ' . PHP_EOL . $e->getMessage();
         }
--- database/migrations/CreateProductsTable.php
+++ database/migrations/CreateProductsTable.php
@@ -21,10 +21,13 @@ class CreateProductsTable
                             weight VARCHAR(40),
                             dimensions VARCHAR(40),
                             product_type_id INT(6) UNSIGNED NOT NULL,
-                            deleted_at TIMESTAMP
-                            )';
+                            deleted_at INT(12)
+                            );
+                            INSERT INTO products VALUES(2,\'GGWP0007\',\'War & Peace\',15,null,\'25\',null,1,null);
+                            INSERT INTO products VALUES(1,\'JVC200123\',\'ACME DISK\',25,\'700MB\',null,null,2,null);
+                            INSERT INTO products VALUES(3,\'TR120555\',\'Chair\',15,null,null,\'25x30x15\',3,null);';
             $connection->exec($createTableSql);
-            echo 'Created Table Products Successfully' . PHP_EOL;
+            echo 'Created Table Products and Inserted Initial Values Successfully' . PHP_EOL;
             $addForeignKeySql = 'ALTER TABLE products
                                  ADD CONSTRAINT FK_ProductType
                                  FOREIGN KEY (product_type_id) REFERENCES product_types(id)';
